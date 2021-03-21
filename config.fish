9965317f6af1aa42d18a988e3df014b9be7d5862
Hiroshi ISHITATE <tetsuo@techfirm.co.jp>
2021-03-21 12:21:59 +0900

    add work around

--- config.fish
+++ config.fish
@@ -6,15 +6,15 @@ set -gx LSCOLORS gxfxcxdxbxegedabagacad
 
 # homebrew
 if test -d /opt/homebrew/bin
-  set -x PATH /opt/homebrew/bin $PATH
+  set -gx PATH /opt/homebrew/bin $PATH
 end
 
 if test -d (brew --prefix)"/share/fish/completions"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+  set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
 end
 
 if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+  set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
 end
 
 # asdf
@@ -22,3 +22,7 @@ if test -f $HOME/.asdf/asdf.fish
   source $HOME/.asdf/asdf.fish
 end
 
+# completions work around
+for file in ~/.config/fish/completions/*.fish
+    source $file
+end
