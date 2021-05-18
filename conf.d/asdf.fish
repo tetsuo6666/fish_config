8dc0a6b745d42f452f1ceaf4f876007e45507a64
Hiroshi ISHITATE <tetsuo6666@gmail.com>
2021-05-18 20:51:50 +0900

    add gcloud

--- conf.d/asdf.fish
+++ conf.d/asdf.fish
@@ -2,3 +2,8 @@
 if test -f $HOME/.asdf/asdf.fish
   source $HOME/.asdf/asdf.fish
 end
+
+if test -f $HOME/.asdf/plugins/java/set-java-home.fish
+  source $HOME/.asdf/plugins/java/set-java-home.fish
+end
+
