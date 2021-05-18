211a1650bcfd49d12c2dc19de7661f8b2d54a0dd
Hiroshi ISHITATE <tetsuo@techfirm.co.jp>
2021-05-19 08:49:56 +0900

    modified gcloud

--- conf.d/gcloud.fish
+++ conf.d/gcloud.fish
@@ -1,3 +1,3 @@
-if test -f /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-  source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+if test -f (brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+  source (brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 end
